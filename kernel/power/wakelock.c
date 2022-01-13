e60e5a3fb6b534282d1b9078e81457884a5e3840
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2022-01-13 19:44:20 +0100

    PM: wakeup: simplify the output logic of pm_show_wakelocks()
    commit c9d967b2ce40d71e968eb839f36c936b8a9cf1ea upstream.
    
    The buffer handling in pm_show_wakelocks() is tricky, and hopefully
    correct.  Ensure it really is correct by using sysfs_emit_at() which
    handles all of the tricky string handling logic in a PAGE_SIZE buffer
    for us automatically as this is a sysfs file being read from.
    
    Reviewed-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/power/wakelock.c
+++ kernel/power/wakelock.c
@@ -39,23 +39,24 @@ ssize_t pm_show_wakelocks(char *buf, bool show_active)
 {
 	struct rb_node *node;
 	struct wakelock *wl;
-	char *str = buf;
-	char *end = buf + PAGE_SIZE;
+	int len = 0;
 
 	mutex_lock(&wakelocks_lock);
 
 	for (node = rb_first(&wakelocks_tree); node; node = rb_next(node)) {
 		wl = rb_entry(node, struct wakelock, node);
+<<<<<<< HEAD
 		if (wl->ws->active == show_active)
 			str += scnprintf(str, end - str, "%s ", wl->name);
+=======
+		if (wl->ws.active == show_active)
+			len += sysfs_emit_at(buf, len, "%s ", wl->name);
+>>>>>>> c2ab8958cfbf... PM: wakeup: simplify the output logic of pm_show_wakelocks()
 	}
-	if (str > buf)
-		str--;
-
-	str += scnprintf(str, end - str, "\n");
+	len += sysfs_emit_at(buf, len, "\n");
 
 	mutex_unlock(&wakelocks_lock);
-	return (str - buf);
+	return len;
 }
 
 #if CONFIG_PM_WAKELOCKS_LIMIT > 0
