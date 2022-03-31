a0d4a1c9004ca9446131bdab87307f0ddc2432e8
Marc Zyngier <marc.zyngier@arm.com>
2022-03-31 19:33:34 +0100

    arm64: arch_timer: Add workaround for ARM erratum 1188873
    commit 95b861a4a6d94f64d5242605569218160ebacdbe upstream.
    
    When running on Cortex-A76, a timer access from an AArch32 EL0
    task may end up with a corrupted value or register. The workaround for
    this is to trap these accesses at EL1/EL2 and execute them there.
    
    This only affects versions r0p0, r1p0 and r2p0 of the CPU.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -42,12 +42,20 @@
 #define ARM64_HAS_DCPOP				21
 #define ARM64_UNMAP_KERNEL_AT_EL0		23
 #define ARM64_HARDEN_BRANCH_PREDICTOR		24
+<<<<<<< HEAD
 #define ARM64_HARDEN_BP_POST_GUEST_EXIT		25
 #define ARM64_SSBD				26
 #define ARM64_MISMATCHED_CACHE_TYPE		27
 #define ARM64_SSBS				28
 
 
+=======
+#define ARM64_SSBD				25
+#define ARM64_MISMATCHED_CACHE_TYPE		26
+#define ARM64_SSBS				27
+#define ARM64_WORKAROUND_1188873		28
+
+>>>>>>> 9aeb4a5a73d3... arm64: arch_timer: Add workaround for ARM erratum 1188873
 #define ARM64_NCAPS				29
 
 #endif /* __ASM_CPUCAPS_H */
